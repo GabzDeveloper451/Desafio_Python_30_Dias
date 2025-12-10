2ed7d4ff64870d7935e4d80257c99d58a61a967d
Gabriel Botelho <g.botelho451@gmail.com>
2025-12-10 18:51:03 -0300

    Update Dia_2.py

--- Dia_2.py
+++ Dia_2.py
@@ -1,29 +1,12 @@
-# Definindo o nome
-nome = input("\nOlá! Digite o seu nome: ")
+# Desafio 2: Crie variáveis para seu nome, idade e altura. Imprima uma frase combinando-as.
 
-# Definindo a idade
-while True:
-    idade = input("\nDigite a sua idade: ")
-    if idade.isdigit():
-        idade = int(idade)
-        break
-    else:
-        print("\n\tERRO: DIGITE UM NÚMERO VÁLIDO.")
-        continue
+# Definindo a variável do nome
+nome = "Gabriel"
 
-# Definindo a altura
-while True:
-    altura = input("\nDigite a sua altura (ex: 1.75): ")
+# Definindo a variável da idade
+idade = 12
 
-    # Verificação de formato: precisa ter ponto e duas casas decimais
-    if "." in altura:
-        parte_inteira, parte_decimal = altura.split(".", 2)
+# Definindo a variável da altura
+altura = 1.57
 
-        # Verifica se a parte decimal tem 2 números e se tudo é numérico
-        if parte_decimal.isdigit() and len(parte_decimal) == 2 and parte_inteira.replace("-", "").isdigit():
-            altura = float(altura)
-            break
-
-    print("ERRO: DIGITE A ALTURA NO FORMATO NUMÉRICO COM DUAS CASAS DECIMAIS. EX: 1.75")
-
-print(f"\nSeu nome é {nome}, você tem {idade} anos de idade e {altura}m de altura.")
+print(f"Olá! Meu nome é {nome}, eu tenho {idade} anos, e tenho {altura} metros de altura.")
