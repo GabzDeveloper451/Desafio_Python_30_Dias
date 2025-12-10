949ebb30787e556a62dd5208f174c887910e5b9e
Gabriel Botelho <g.botelho451@gmail.com>
2025-12-10 17:48:45 -0300

    Create Dia_1.py

---
+++ Dia_1.py
@@ -0,0 +1,7 @@
+# Desafio 1: Sintaxe Básica - Escreva um programa que imprima "Olá, Mundo!" e seu nome em linhas separadas.
+
+# Salvando o nome do usuário
+nome = input('Digite o seu nome: ')
+
+# Imprimindo "Olá, Mundo!"
+print(f"\nOlá, Mundo! Meu nome é {nome}")
