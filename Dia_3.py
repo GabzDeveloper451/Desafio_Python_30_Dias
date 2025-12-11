f4f51d92e2d3ef3c0bfe14da81cee4dfb32a0d03
Gabriel Botelho <g.botelho451@gmail.com>
2025-12-11 11:05:07 -0300

    Create Dia_3.py

---
+++ Dia_3.py
@@ -0,0 +1,38 @@
+# Desafio 3: Calcule o Índice de Massa Corporal (IMC) usando entradas de peso e altura.
+
+# Imprimindo uma mensagem de entrada
+print("Olá, com esse programa, calcularei o seu Índice de Massa Corporal (IMC) para isso: ")
+
+# Salvando o peso do usuário
+
+# Garantindo que o usuário digite apenas o valor numérico de seu peso
+while True:
+    peso = input("\nDigite quanto você pesa ex(69.2): ")
+    try:
+        peso = float(peso)
+        if peso <= 0:
+            print("Por favor, digite um valor maior que zero.")
+            continue
+        break
+    except ValueError:
+        print("Por favor, digite apenas o valor numérico de seu peso.")
+
+# Salvando a altura do usuário
+while True:
+    altura = input("\nDigite a sua altura ex(1.75): ")
+    try:
+       altura = float(altura)
+       if altura <= 0:
+           print("Por favor, digite um valor maior que zero.")
+           continue
+       break
+    except ValueError:
+        print("Por favor, digite apenas o valor numérico de sua altura.")
+# Salvando o cálculo do IMC do usuário
+imc = peso / (altura ** 2)
+
+# Imprimino o IMC do usuário
+print(
+    "\n Calculando..."
+    f"\n O seu Índice de Massa corporal é de {imc:.2f} kg/m²"
+)
