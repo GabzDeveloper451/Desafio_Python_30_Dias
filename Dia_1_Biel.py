d4b7a1f05a042ada446aa2a726ca4d80947a9482
Gabriel Botelho <g.botelho451@gmail.com>
2025-12-17 09:42:10 -0300

    Update Dia_1_Biel.py

--- Dia_1_Biel.py
+++ Dia_1_Biel.py
@@ -1,5 +1,7 @@
 # Desafio 1: Escreva um programa que imprima "Olá, Mundo!" e seu nome em linhas separadas.
 
+print("\n--- Desafio 1 ---\n")
+
 # Imprimindo a mensagem na tela
 print(
     "Olá, Mundo!\n"
