c958e5535b9e968785d0c8136630041f9647a527
Gabriel Botelho <g.botelho451@gmail.com>
2025-12-19 10:59:07 -0300

    Update Dia_11_Biel.py

--- Dia_11_Biel.py
+++ Dia_11_Biel.py
@@ -3,7 +3,7 @@
 # Mensagens de entrada:
 print("\n--- Desafio 11 ---\n")
 
-print("\tNesse programa você digitará dois números e eu lhe mostrarei qual é o maior entre os dois.\n")
+print("\tNesse programa você digitará dois números e eu lhe mostrarei qual é o maior entre os dois.")
 # Definindo a função:
 def maior_num(x, y):
     if x > y:
@@ -11,10 +11,17 @@ def maior_num(x, y):
     else:
         return y
 
+while True:
+    # Definindo o primeiro e o segundo número
+    num_1 = input("\nDigite o primeiro número: ")
+    num_2 = input("\nDigite o segundo número: ")
+    try:
+        float(num_1)
+        float(num_2)
+    
+    except:
+        print("\n\vERRO: Você não digitou um número.")
+        continue
 
-# Definindo o primeiro e o segundo número
-num_1 = input("Digite o primeiro número: ")
+    print(f"\n\tO maior número entre {num_1} e {num_2} é o número: {maior_num(num_1,num_2)}")
 
-num_2 = input("\nDigite o segundo número: ")
-
-print(f"\n\tO maior número entre {num_1} e {num_2} é o número: {maior_num(num_1,num_2)}")
