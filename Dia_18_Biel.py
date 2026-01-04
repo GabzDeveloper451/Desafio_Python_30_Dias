29601d5433513f32c52ceeba140f3d61cc882b67
Gabriel Botelho <gabrielbotelho451@gmail.com>
2026-01-04 14:00:48 -0300

    Create Dia_18_Biel.py

---
+++ Dia_18_Biel.py
@@ -0,0 +1,12 @@
+# Desafio 18: Adicione uma nova chave, atualize um valor e use .keys() e .values() para imprimir as chaves e valores separadamente.
+
+# Criando outro dict:
+dict = {"Name": "Gabriel", "Age": 12, "City": "São Paulo"}
+
+# Imprimindo as chaves, e os valores separadamente
+
+# Imprimindo as chaves
+print(f"Chaves do dicionário: {tuple(dict.keys())}")
+
+# Imprimino os valores das chaves
+print(f"Valores das chaves: {tuple(dict.values())}")
