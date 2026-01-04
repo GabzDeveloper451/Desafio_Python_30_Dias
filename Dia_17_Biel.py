79bac34b31345f16dbb3737a37eb26ff0e67e458
Gabriel Botelho <gabrielbotelho451@gmail.com>
2026-01-04 13:54:16 -0300

    Create Dia_17_Biel.py

---
+++ Dia_17_Biel.py
@@ -0,0 +1,13 @@
+# Desafio 17: Crie um dicionário para armazenar informações de uma pessoa (nome, idade, cidade).
+
+# Salvando o dicionário
+dict = {'Nome': 'Gabriel', 'Idade': 12, 'Cidade': 'São Paulo'}
+
+# Imprimindo o Nome do usuário
+print(f"Nome: {dict['Nome']}")
+
+# Imprimindo a Idade do usuário
+print(f"Idade: {dict['Idade']}")
+
+# Imprimindo a Cidade a qual o usuário reside
+print(f"Cidade: {dict['Cidade']}")
